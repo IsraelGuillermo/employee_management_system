a8dd1347555c2d88361ca0f0154a127a15ef5b22
Israel Guillermo <israelguillermo22@gmail.com>
2020-12-08 22:54:29 -0600

    updating comments, readme and functionality to also view departments and roles listed.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # employee_management_system
+
+# Employee Management System
+
+## Table of Contents
+
+- [Description](#desciption)
+- [Installation](#installation)
+- [Usage](#Usage)
+- [Contribution](#Contribution)
+- [Test](#test)
+- [License](#license)
+
+## Description
+
+![Application](./images/application.png)
+
+<br />
+This is a back end program which allows user to use the terminal to manage employees. The program has several functionalities which include, adding an employee, role and department. The program also allows the user to view employees, departments and roles. Additionally the program allows the user to view employees by roles and departments. Lastly this program allows the user to update the role of an employee. <br /> This program uses node, inquirer, console.table and mysql packages which provides the user the ease of use. <br /> The program stores all the data in a database by using mysql. when reading, updating or creating information the program pushes this to the 3 tables (employee, role, and department) found on the database.
+
+## License
+
+![badge](https://img.shields.io/badge/license=None-green)
+<br />
+None
+
+## Installation
+
+npm i
+
+## Usage
+
+1. Open integrated terminal <br /> 2. Run node index.js 3. Follow the questions being prompted.
+
+## Contribution
+
+None
+
+## Test
+
+1. Open integrated terminal <br /> 2. Run node index.js <br />3. Follow the questions being prompted.
+
+## For any additional questions please contact me at:
+
+Email: israelguillermo22@gmail.com
+<br />
+GitHub: [IsraelGuillermo](https://github.com/IsraelGuillermo)
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+// These variables  pull functions from the lib folder
 const { addEmployee, addDepartment, addRole } = require("./lib/add");
-const { viewEmployees, viewByDepartments, viewByRoles } = require("./lib/view");
+const {
+  viewDepartments,
+  viewEmployees,
+  viewByDepartments,
+  viewByRoles,
+  viewRoles,
+} = require("./lib/view");
 const { updateEmployeeRoles } = require("./lib/update");
+
+// This is the first question which will be asked when the program is ran
 const firstQuestion = {
   name: "firstQuestion",
   type: "list",
@@ -12,12 +22,16 @@ const firstQuestion = {
     "Add Department",
     "Add Role",
     "View Employees",
+    "View Roles",
+    "View Departments",
     "View By Departments",
     "View By Roles",
     "Update Employee Role",
     "Quit",
   ],
 };
+
+// Creating conenection to mySQL
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -27,10 +41,13 @@ const connection = mysql.createConnection({
   database: "employee_managementDB",
 });
 
+// Once connected program is initiated by calling the init() function.
 connection.connect(function (err) {
   if (err) throw err;
   init();
 });
+
+//  Function intiates program and ask what the user would like to do, based on response the program will run different functions.
 
 function init() {
   inquirer.prompt(firstQuestion).then((response) => {
@@ -56,6 +73,12 @@ function init() {
       case "Update Employee Role":
         updateEmployeeRoles(connection, init);
         break;
+      case "View Roles":
+        viewRoles(connection, init);
+        break;
+      case "View Departments":
+        viewDepartments(connection, init);
+        break;
       case "Quit":
         quit();
         break;
@@ -63,6 +86,7 @@ function init() {
   });
 }
 
+// This function ends the program if user selects to quit
 function quit() {
   connection.end();
 }
